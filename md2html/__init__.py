d4c8c5aef6e8f950f5f950d0e81ee8d102a5454c
Phuker <Phuker@users.noreply.github.com>
2020-10-26 22:58:56 +0800

    Bump version to 0.1.3

--- md2html/__init__.py
+++ md2html/__init__.py
@@ -29,7 +29,7 @@ import gfm
 
 from css_html_js_minify import css_minify, html_minify
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 logging_stream = sys.stderr
 logging_format = '\033[1m%(asctime)s [%(levelname)s]:\033[0m%(message)s'
